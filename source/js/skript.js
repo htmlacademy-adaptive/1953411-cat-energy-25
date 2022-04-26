c4d5afadc1018ede413553fde49d855fcc06f295
timeliercomb <levshin.v.99@gmail.com>
2022-04-26 09:37:52 +0300

    добавляет мелкие сетки блоку presentation

--- source/js/skript.js
+++ source/js/skript.js
@@ -1,7 +1,7 @@
 let navMain = document.querySelector('.main-navigation');
 let navToggle = document.querySelector('.main-navigation__toggle');
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-navigation--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-navigation--closed')) {
